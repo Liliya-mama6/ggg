76eb344b712c86b553dcb79e8155368d8f978add
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-09-17 21:30:45 +0500

    Update sirius.p

--- sirius.py
+++ sirius.py
@@ -1,49 +1,8 @@
-from pprint import pprint
-class Product:
-    def __init__(self, name, weight, category):
-        self.name=name
-        self.weight=weight
-        self.category=category
-
-    def __str__(self):
-        return f'{self.name}, {str(self.weight)}, {self.category}'
-
-class Shop:
-    __file_name='products.txt'# не получилось создать код который сам создает файл и при этом не перезаписывает список продуктов так что нужно создать файл заранее
-    def add(self, *products):
-        for i in range(len(products)):
-            file=open(self.__file_name, 'r')
-            s=file.read()
-            file.close()
-            if s=='':
-                file=open(self.__file_name, 'w')
-                file.write(str(products[i]))
-                file.close()
-            elif s!='' and s.count(str(products[i]))==0:
-                file = open(self.__file_name, 'a')
-                file.write('\n'+str(products[i]))
-                file.close()
-            elif s!='' and s.count(str(products[i]))>0:
-                print(f'Продукт {str(products[i].name)} уже есть в магазине')
-
-    def get_products(self):
-        file = open(self.__file_name, 'r')
-        s = file.read()
-        file.close()
-        file = open(self.__file_name, 'w')
-        file.write(s)
-        file.close()
-        return s
-
-
-s1 = Shop()
-p1 = Product('Potato', 50.5, 'Vegetables')
-p2 = Product('Spaghetti', 3.4, 'Groceries')
-p3 = Product('Potato', 5.5, 'Vegetables')
-
-print(p2) # __str__
-
-s1.add(p1, p2, p3)
-print(s1.get_products())
-file=open('products.txt', 'r')
-file.read()
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+first_result=[len(i) for i in first_strings if len(i)>5]
+second_result=[(i, j) for i in first_strings for j in second_strings if len(i)==len(j)]
+third_result={i:len(i) for i in first_strings+second_strings if not len(i)%2}
+print(first_result)
+print(second_result)
+print(third_result)
