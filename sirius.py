62380a7eed41c1b9e3f5bcf634abe34f9cd80aa7
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-11-21 17:37:46 +0500

    переделанное дз

--- sirius.py
+++ sirius.py
@@ -13,19 +13,22 @@ async def get() -> dict:
 
 
 @app.post('/user/{username}/{age}')
-async def create(username, age) -> str:
+async def create(username:str=Path(min_length=5, max_length=20, description='Enter username', example='urban'),
+                 age:int=Path(ge=18, le=120, description='EnterUSER age', example='22')) -> str:
     a=str(int(max(users, key=int))+1)
     users[a]=f"Имя: {str(username)}, возраст: {str(age)}"
     return f"User {a} is registered"
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update(user_id, username, age) ->str:
+async def update(user_id:int = Path(ge=1, le=100, description='EnterUSER id', example='12'),
+                 username:str=Path(min_length=5, max_length=20, description='Enter username', example='urban'),
+                 age:int=Path(ge=18, le=120, description='Enter age', example='119')) ->str:
     users[user_id]=f'Имя: {str(username)}, возраст: {str(age)}'
     return f"The user {user_id} is updated"
 
 
 @app.delete('/user/{user_id}')
-async def delete(user_id)->str:
+async def delete(user_id:int=Path(ge=1, le=100, description='EnterUSER id', example='12'))->str:
     users.pop(user_id)
     return f'The user {user_id} was deleted'
